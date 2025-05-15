b9b011fd7da22d43373b5e5988e3a809955b0de7
Michael Bialowas <mbialowas@rrc.ca>
2025-05-15 11:18:58 -0500

    feature: Added bottom navigation to the app.

--- app/src/main/java/com/mbialowas/bottomnavdemo_spr_int/MainActivity.kt
+++ app/src/main/java/com/mbialowas/bottomnavdemo_spr_int/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.mbialowas.bottomnavdemo_spr_int
 
+
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    // variable declaration
+    lateinit var bottomNavBar: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNavBar)
+
+        bottomNavBar.setOnItemSelectedListener{ menuItem->
+            when(menuItem.itemId){
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_search -> replaceFragment(SearchFragment())
+                R.id.nav_profile -> replaceFragment(ProfileFragment())
+            }
+            true
+        }
+    }
+    private fun replaceFragment(fragment: Fragment){
+        Log.i("MJB", "fragment method being called: ${fragment}")
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer,fragment)
+            .commit()
     }
 }
